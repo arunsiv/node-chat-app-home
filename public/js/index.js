a43a4952251e2de99e662ea70cc86b1627280a72
Arun <Arun>
2017-04-18 21:28:23 -0400

    added autoscroll

--- public/js/index.js
+++ public/js/index.js
@@ -1,18 +1,89 @@
 var socket = io();
 
+function scrollToBottom() {
+    //Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+
+    //heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if ((clientHeight + scrollTop + newMessageHeight + lastMessageHeight) >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+};
+
 socket.on('connect', function() {
     console.log('Connected to server');
-
-    socket.emit('createMessage', {
-        to: 'Dana',
-        text: 'Hey, Dana'
-    });
-});
-
-socket.on('newMessage', function(message) {
-    console.log('New Message: ', message);
 });
 
 socket.on('disconnect', function() {
     console.log('Disconnected from server');
+});
+
+//Listener for new message
+socket.on('newMessage', function(message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#message-template').html();
+
+    var html = Mustache.render(template, {
+        from: message.from,
+        text: message.text,
+        createdAt: formattedTime
+    });
+
+    jQuery('#messages').append(html);
+    scrollToBottom();
+});
+
+//Listener for new location message
+socket.on('newLocationMessage', function(message) {
+    var formattedTime = moment(message.createdAt).format('h:mm a');
+    var template = jQuery('#location-message-template').html();
+
+    var html = Mustache.render(template, {
+        from: message.from,
+        url: message.url,
+        createdAt: formattedTime
+    });
+
+    jQuery('#messages').append(html);
+    scrollToBottom();
+});
+
+//Get the text entered by user and send it to server
+jQuery('#message-form').on('submit', function(e) {
+    e.preventDefault();
+    var messageTextBox = jQuery('[name=message]');
+
+    socket.emit('createMessage', {
+        from: 'User',
+        text: messageTextBox.val()
+    }, function() {
+        messageTextBox.val('');
+    });
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function() {
+    if (!navigator.geolocation) {
+        return alert('Geolocation not support by your browser...');
+    }
+
+    locationButton.attr('disabled', 'disabled').text('Sending Location...');
+
+    navigator.geolocation.getCurrentPosition(function(position) {
+        locationButton.removeAttr('disabled').text('Send Location');
+        socket.emit('createLocationMessage', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        });
+    }, function() {
+        alert('Unable to fetch location...');
+        locationButton.removeAttr('disabled').text('Send Location');
+    });
 });
--- server/server.js
+++ server/server.js
@@ -3,30 +3,52 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const { generateMessage, generateLocationMessage } = require('./utils/message');
+
 const publicPath = path.join(__dirname, '../public');
 const port = process.env.PORT || 3000;
 
 var app = express();
+
+//Use socket.io with server
 var server = http.createServer(app);
 var io = socketIO(server);
 
 //Static Middleware
 app.use(express.static(publicPath));
 
-//
+//Socket IO events
+//Connection event
 io.on('connection', (socket) => {
     console.log('User connected');
 
-    socket.emit('newMessage', {
-        from: 'Dana',
-        text: 'Hey, Arun',
-        createdAt: '123abc'
-    });
+    //Send welcome message when a new user joins
+    socket.emit('newMessage', generateMessage('Admin', 'Welcome to Chat App!!!'));
 
-    socket.on('createMessage', function(newMessage) {
+    //Broadcast when a new user joins
+    socket.broadcast.emit('newMessage', generateMessage('Admin', 'New user joined...'));
+
+    //Listen for new message from user
+    socket.on('createMessage', (newMessage, callback) => {
         console.log('New Message: ', newMessage);
+
+        //Send the new message to all the users connected to the server
+        //IO.emit emits the event to every client
+        io.emit('newMessage', generateMessage(newMessage.from, newMessage.text));
+
+        //Ack from server
+        callback();
     });
 
+    //Listen for new location message from user
+    socket.on('createLocationMessage', (coords) => {
+        console.log('New Location Message: ', coords);
+
+        //Send the new message to all the users connected to the server
+        io.emit('newLocationMessage', generateLocationMessage('Admin', coords.latitude, coords.longitude));
+    });
+
+    //Disconnet event
     socket.on('disconnect', () => {
         console.log('User disconnected');
     });
